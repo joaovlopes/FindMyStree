90628a368ad373b4c4be46f8e268a0e7d6ca2f01
João Lopes <joaovlopesmartins@gmail.com>
2024-12-24 10:21:17 -0300

    feat: add header component

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div>
+        <h1>
+            Find<span>My</span>Street
+        </h1>
+    </div>
+</template>
+
+<style scoped lang="scss">
+
+@use '../styles/variables.scss' as *;
+
+ div {
+    width: 100%;
+    text-align: center;
+ }
+
+ h1 {
+    color: $primary-color;
+    font-family: 'Righteous', cursive;
+ }
+
+ span {
+    color: $white-color;
+ }
+</style>
